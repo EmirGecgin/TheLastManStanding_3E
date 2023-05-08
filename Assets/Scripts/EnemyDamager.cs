3120ba718a1ddb993956599c1e9038f6631c3b4e
EGE\copro <egekula3@gmail.com>
2023-05-08 16:03:45 +0300

    +EnemyDamager Script updated. +FireballWeapon Script bug fixed.

--- Assets/Scripts/EnemyDamager.cs
+++ Assets/Scripts/EnemyDamager.cs
@@ -6,7 +6,32 @@ using UnityEngine;
 public class EnemyDamager : MonoBehaviour
 {
     [SerializeField] private float damageAmount;
-    
+
+    [SerializeField] private float lifeTime,growSpeed = 5f;
+    private Vector3 _targetSize;
+
+    private void Start()
+    {
+        _targetSize = transform.localScale;
+        transform.localScale = Vector3.zero;
+    }
+
+    private void Update()
+    {
+        transform.localScale = Vector3.MoveTowards(transform.localScale, 
+            _targetSize, growSpeed * Time.deltaTime);
+        lifeTime -= Time.deltaTime;
+        if (lifeTime <= 0)
+        {
+            _targetSize = Vector3.zero;
+            if (transform.localScale.x == 0f)
+            {
+                Destroy(gameObject);
+            }
+        }
+
+    }
+
 
     private void OnTriggerEnter2D(Collider2D col)
     {
--- Assets/Scripts/Weapons/FireballWeapon.cs
+++ Assets/Scripts/Weapons/FireballWeapon.cs
@@ -5,16 +5,24 @@ using UnityEngine;
 public class FireballWeapon : MonoBehaviour
 {
     [SerializeField] private float rotationSpeed;
-    [SerializeField] private Transform fireballHolder;
-    void Start()
-    {
-        
-    }
+    [SerializeField] private Transform fireballHolder,fireballToSpawn;
+
+    [SerializeField] private float timeBetweenSpawn;
+    private float _spawnCounter;
+    
 
     
     void Update()
     {
         fireballHolder.transform.rotation =
             Quaternion.Euler(0f, 0f, fireballHolder.transform.rotation.eulerAngles.z + (rotationSpeed * Time.deltaTime));
+
+        _spawnCounter -= Time.deltaTime;
+        if (_spawnCounter <= 0)
+        {
+            _spawnCounter = timeBetweenSpawn;
+            
+            Instantiate(fireballToSpawn,fireballToSpawn.position,fireballToSpawn.rotation,fireballHolder).gameObject.SetActive(true);
+        }
     }
 }
